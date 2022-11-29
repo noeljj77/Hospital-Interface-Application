3c223d84719ccaf05e5d4174473d2bc1e2098805
no'L <92153680+noeljj77@users.noreply.github.com>
2022-11-29 21:21:37 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,59 @@
+# Modules
+
 import mysql.connector
 import webview
 
-mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "****", database = "Hospital")  
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# MySQL Connection
+
+mydb = mysql.connector.connect(host = "localhost", user = "root", passwd = "noel", database = "Hospital")  
 mycursor = mydb.cursor()
 
-'''#DOCTOR TABLE:
-mycursor.execute("CREATE TABLE Doctor(Doc_ID int(4) primary key , Name varchar(100) not null , Department varchar(50) not null)") 
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# Tables
+
+#DOCTOR TABLE:
+#mycursor.execute("CREATE TABLE Doctor(Doc_ID int(4) primary key , Name varchar(100) not null , Department varchar(50) not null)") 
 
 #APPOINTMENT TABLE:
-mycursor.execute("CREATE TABLE Appointments(App_ID int(4) primary key, Department varchar(30), Doctor varchar(100), Patient_Name varchar(100), Mobile_No int, Date date, Time varchar(10),Patient_Mail varchar(50))");
+#mycursor.execute("CREATE TABLE Appointments(App_ID int(4) primary key, Department varchar(30), Doctor varchar(100), Patient_Name varchar(100), Mobile_No int, Date date, Time varchar(10),Patient_Mail varchar(50))");
 
 #USERINFO TABLE:
-mycursor.execute("CREATE TABLE UserInfo(Patient_Mail varchar(50) primary key, Password varchar(50))")'''
+#mycursor.execute("CREATE TABLE UserInfo(Patient_Mail varchar(50) primary key, Password varchar(50))")
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# Main
 
 class api:  
 
     res_id = 0
 
-    # LOGIN MENU:
-
-    # PATIENT LOGIN:
+# PATIENT LOGIN
     def user_menu(self,usermail,userpass):
         try:
             mycursor.execute("SELECT Password FROM UserInfo WHERE Patient_Mail = '{}'".format(usermail))
             result = mycursor.fetchall()
+
             if result[0][0] == userpass:
                 return True
             else:
                 return False
+
         except:
             return False
-    # PATIENT REGISTER:
+
+
+# PATIENT REGISTER
     def patient_register(self,patient_mail,patient_pass):
         try:
             mycursor.execute("SELECT Patient_Mail FROM UserInfo WHERE Patient_Mail = '{}'".format(patient_mail))
             result = mycursor.fetchall()
+
             if "@" in str(patient_mail) and ".com" in str(patient_mail):
+
                 if len(result) == 0:
                     mycursor.execute("INSERT INTO UserInfo(Patient_Mail, Password) VALUES ('{}','{}')".format(patient_mail,patient_pass))
                     mydb.commit()
@@ -44,43 +62,53 @@ class api:
                 else:
                     l = ['0',"Email already exists"]
                     return l
+
             else:
                 l = ['1',"Invalid Email"]
                 return l
+
         except:
             return False
-    # ADMIN LOGIN:
+
+
+# ADMIN LOGIN
     def admin_menu(self,password):
+
         if password == "admin123": 
             return True
         else:
             return False
 
 
-    # ADMIN MENU FUNCTIONS
-
-    # NEW DOCTOR
+# ADD NEW DOCTOR
     def add_doctor(self,doc_name,doc_department):
         mycursor.execute("SELECT Doc_ID FROM Doctor")
         result = mycursor.fetchall()
+
         if len(result) == 0:
             doc_id = 1
         else:
             doc_id = result[-1][0]+1
+
         mycursor.execute("INSERT INTO Doctor(Doc_ID, Name, Department) VALUES ({},'{}','{}')".format(doc_id, doc_name, doc_department))
         mydb.commit()
         return True
-    # VIEW DOCTORS IN A DEPARTMENT
+
+
+# VIEW DOCTORS IN A DEPARTMENT
     def view_doctors(self,department):
         mycursor.execute("SELECT Name FROM Doctor where Department = '{}'".format(department))
         result = mycursor.fetchall()
         return list(result)
-    # VIEW APPOINTMENTS OF A DOCTOR
+
+
+# VIEW APPOINTMENTS OF A DOCTOR
     def view_appointments_doctor(self,doctor_name):
         html=""
         try:
             mycursor.execute("SELECT * FROM Appointments WHERE Doctor = '{}'".format(doctor_name))
             result = mycursor.fetchall()
+
             for i in result:
                 html = html + '''<li>
                 <div class = "Doctor_Column_Header">
@@ -108,13 +136,17 @@ class api:
                 </div>
                 </li>'''.format(i[0],i[1],i[2],i[3],i[4],i[5],i[6])
             return html
+
         except Exception as e:
             print(e)
-    # VIEW ALL DOCTORS
+
+
+# VIEW ALL DOCTORS
     def view_all_doctors(self):
         html = ""
         mycursor.execute("SELECT * FROM Doctor")
         result = mycursor.fetchall()
+
         for i in result:
             html = html + '''<li>
               <div class = "Doctor_Column_Header">
@@ -132,43 +164,53 @@ class api:
                 </div>
             </div>
             </li>'''.format(i[0],i[1],i[2],i[0])
+
         return html
-    # DELETE DOCTOR
+
+
+# DELETE DOCTOR
     def delete_doctor(self,doc_id):
         mycursor.execute("DELETE FROM Doctor WHERE Doc_ID = {}".format(doc_id))
         mydb.commit()
         return True
     
 
-    # PATIENT MENU FUNCTIONS:
-
-    # BOOK APPOINTMENT
+# BOOK APPOINTMENT
     def book_appointment(self,department,doctor_name,patient_name,mob_no,date,time,patient_mail):
         mycursor.execute("SELECT App_ID FROM Appointments")
         result = mycursor.fetchall()
+
         if len(result) == 0:
             app_id = 1
+
         else:
             app_id = result[-1][0]+1
         mycursor.execute("SELECT * FROM Appointments WHERE Patient_Mail = '{}'".format(patient_mail))
         result = mycursor.fetchall()
+
         if len(result) == 0:
             mycursor.execute("INSERT INTO Appointments(App_ID, Department, Doctor, Patient_Name, Mobile_No, Date, Time, Patient_Mail) VALUES({},'{}','{}','{}','{}','{}','{}','{}')".format(app_id,department,doctor_name,patient_name,mob_no,date,time,patient_mail))
             mydb.commit()
             return True
+
         else:
             l = ["1","You already have a pending appointment"]
             return l
-    # VIEW DEPARTMENTS
+
+
+# VIEW DEPARTMENTS
     def view_departments(self):
         mycursor.execute("SELECT DISTINCT Department FROM Doctor")
         result = mycursor.fetchall()
         return list(result)
-    # VIEW APPOINTMENTS
+
+
+# VIEW APPOINTMENTS
     def view_appointments(self,patient_mail):
         html = ""
         mycursor.execute("SELECT * FROM Appointments WHERE Patient_Mail = '{}'".format(patient_mail))
         result = mycursor.fetchall()
+
         if len(result) != 0:
             for i in result:
                 dte = str(i[5])
@@ -187,36 +229,42 @@ class api:
                     <h3>Date: {}</h3>
                     <br><br>
                     <h3>Time: {}</h3>
-                    <br><br>
-                    
+                    <br><br>                    
                     <button class="card-btn" onclick="res_app();">RESCHEDULE APPOINTMENT</button>
                     <button class="card-btn" onclick="cancel_app();">CANCEL APPOINTMENT</button>
-                    </div>
-                
+                    </div>                
                 </div>
                 '''.format(i[3],i[0],i[1],i[2],dte,i[6])
                 global res_id
                 res_id = i[0]
             return html
+
         else:
             l = ["1","You have no appointments"]
             return l
-    # RESCHEDULE APPOINTMENT
+
+
+# RESCHEDULE APPOINTMENT
     def reschedule_appointment(self,date,time):
         global res_id
         mycursor.execute("UPDATE Appointments SET Date = '{}', Time = '{}' WHERE App_ID = {}".format(date,time,res_id))
         mydb.commit()
-    # CANCEL APPOINTMENT
+
+
+# CANCEL APPOINTMENT
     def cancel_appointment(self):
         global res_id
         mycursor.execute("DELETE FROM Appointments WHERE App_ID = {}".format(res_id))
         mydb.commit()
         
+
 api = api()
-with open("Hospital.html", "r") as f:
+with open("Medlife.html", "r") as f:
+
     try:
         r = f.read()
-        webview.create_window(title='Medlife Healthcare', html=r, js_api=api,fullscreen=True)
+        webview.create_window(title='Medlife Healthcare', html=r, js_api=api)
         webview.start(debug=True)
+
     except Exception as E:
         print(E)
